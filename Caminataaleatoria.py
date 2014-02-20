2cb94816c97e8a4a26100eb8af4068d271ead718
users <js.calderon3227@compufi14.uniandes.edu.co>
2014-02-20 13:51:14 -0500

    esfera uniforme

--- Caminataaleatoria.py
+++ Caminataaleatoria.py
@@ -1,26 +1,26 @@
 import numpy as np
-import mathplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import sys
 
 n=sys.argv[1]
 
 numero=int(n)
 
-theta = random.random(numero)
-deltax=cos(2*pi*theta)
-deltay=sin(2*pi*theta)
+theta =np.random.random(numero)
+deltax=np.cos(2*np.pi*theta)
+deltay=np.sin(2*np.pi*theta)
 
-x=zeros(numero)
-y=zeros(numero)
-distancia=zeros(numero)
+x=np.zeros(numero)
+y=np.zeros(numero)
+distancia=np.zeros(numero)
 
 for i in range(1,numero):
     x[i]=x[i-1]+deltax[i-1]
     y[i]=y[i-1]+deltay[i-1]
 
 for i in range(numero):
-    distancia[i]=sqrt((x[i]**2)+(y[i]**2))
+    distancia[i]=np.sqrt((x[i]**2)+(y[i]**2))
 
-numeropasos=numeropasos+ones(numero)
+numeropasos=numeropasos+np.ones(numero)
 
-plot(numeropasos,distancia)
+plt.pyplot.show(numeropasos,distancia)
