95e98cf8bb19086af581b0952fed76767ec08c34
SystemManager <systemmanager@fiscomputo.(none)>
2014-02-26 17:28:02 -0500

    n fotones saliendo del centro del sol

--- fotone.py
+++ fotone.py
@@ -13,20 +13,28 @@ for j in range(numero):
 
   x=0
   y=0
+  z=0
   distancia=0
+
   while distancia<100:
 
       theta =np.random.random(1)
-      deltax=np.cos(2*np.pi*theta)
-      deltay=np.sin(2*np.pi*theta)
+      phi=np.random.random(1)
+      deltax=np.cos(2*np.pi*theta)*np.sin(np.pi*phi)
+      deltay=np.sin(2*np.pi*theta)*np.sin(np.pi*phi)
+      deltaz=np.cos(np.pi*phi)
       
 
       x=x+deltax[0]
       y=y+deltay[0]
-      distancia=np.sqrt((x**2)+(y**2))
+      z=z+deltaz[0]
+      distancia=np.sqrt((x**2)+(y**2)+(z**2))
       numeropasossalir[j]+=1
 
 
 
 plt.hist(numeropasossalir,bins=20)
+plt.savefig("n_fotones_centro.pdf")
+
+
 plt.show()
