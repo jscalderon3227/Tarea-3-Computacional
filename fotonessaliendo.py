79cc442873fd4f5a2a36c158a615d21529555571
users <js.calderon3227@compufi19.uniandes.edu.co>
2014-02-27 16:42:11 -0500

    fotones saliendo esfera homogenea

--- fotonessaliendo.py
+++ fotonessaliendo.py
@@ -9,14 +9,15 @@ numero=int(n)
 numeropasossalir=np.zeros(numero)
 
 for j in range(numero):
+
   asim=np.random.random()
   angulo=np.random.random()
   radio=np.random.random()
   
 
-  r=100*(radio**(1/3))
-  asym=np.arccos((2*asim)-1)
-  polar=2*np.pi*angulo
+  r=100.0*(radio**(1.0/3.0))
+  asym=np.arccos((2.0*asim)-1.0)
+  polar=2.0*np.pi*angulo
 
   x=r*np.sin(asym)*np.cos(polar)
   y=r*np.sin(asym)*np.sin(polar)
@@ -30,21 +31,22 @@ for j in range(numero):
 
       theta =np.random.random(1)
       phi=np.random.random(1)
-      deltax=np.cos(2*np.pi*theta)*np.sin(np.pi*phi)
-      deltay=np.sin(2*np.pi*theta)*np.sin(np.pi*phi)
+      deltax=np.cos(2.0*np.pi*theta)*np.sin(np.pi*phi)
+      deltay=np.sin(2.0*np.pi*theta)*np.sin(np.pi*phi)
       deltaz=np.cos(np.pi*phi)
       
 
-      x=x+deltax[0]
-      y=y+deltay[0]
-      z=z+deltaz[0]
+      x+=deltax[0]
+      y+=deltay[0]
+      z+=deltaz[0]
+
       distancia=np.sqrt((x**2)+(y**2)+(z**2))
       numeropasossalir[j]+=1
 
 
 
-plt.hist(numeropasossalir,bins=20)
-plt.savefig("n_fotones_centro.pdf")
+plt.hist(numeropasossalir,bins=50)
+plt.savefig("n_fotones_uniforme.pdf")
 
 
 plt.show()
